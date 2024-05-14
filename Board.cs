5ad568b5bf182a31a2923f55734b72cd3f71d269
YianOAO <wjh0908178837@gmail.com>
2024-05-15 01:41:47 +0800

    verson1加入專案檔案。

---
+++ Board.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 五子棋
+{
+    internal class Board
+    {
+        public bool CanBePlaced(int x, int y)
+        {
+
+        }
+    }
+}
---
+++ Piece.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;//
+using System.Drawing;
+
+namespace 五子棋
+{
+    abstract class Piece : PictureBox 
+    {
+        public Piece(int x,int y)
+        {
+            this.BackColor = Color.Transparent;
+            this.Location = new Point(x, y);//建立一個點
+            this.Size = new Size(50,50);
+
+        }
+    }
+}
